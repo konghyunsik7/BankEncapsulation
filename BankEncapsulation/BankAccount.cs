18d2f75707564abdd5e86a14ca3aca8f8108c0e3
Hyun Kong <konghyunsik7@gmail.com>
2022-07-14 15:43:36 -0400

    inital

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a new Console Application in your repos folder
+//name it BankEncapsulation
+//Create a new public class named BankAccount
+//In your BankAccount class, create the following:
+//A private field of type double named balance with a value of 0
+//Define a method named Deposit that will accept a double and store that value in the balance field
+//Define a method named GetBalance that will return the amount stored in the balance field
+//In the main method of your application, create a new instance of the BankAccount class. Allow the user of the application to Deposit money and retrieve their balance through the console.
+//Once finished save, commit, and push back to Github
+
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double inputBalance)
+        {
+            balance += inputBalance;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public void Withdrawl(double withDrawlAmount) 
+        {
+            balance -= withDrawlAmount;
+        }
+
+        
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using BankEncapsulation;
+
+
+
+var bankaccount1 = new BankAccount();
+
+Console.WriteLine($"Hello, Your balance is: ${bankaccount1.GetBalance()}");
+Console.WriteLine("How Much would you like to deposit?");
+double x = double.Parse(Console.ReadLine());
+bankaccount1.Deposit(x);
+Console.WriteLine($"Hello, Your NEW balance is: ${bankaccount1.GetBalance()}");
+Console.WriteLine("How Much would you like to withdrawl?");
+double y = double.Parse(Console.ReadLine());
+bankaccount1.Withdrawl(y);
+Console.WriteLine($"Hello, Your NEW balance is: ${bankaccount1.GetBalance()}");
